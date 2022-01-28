32132f4efa390259cff4c674f9c305535f53fb7c
Ilia <agi.developr@gmail.com>
2022-01-28 08:12:44 -0500

    adding comments

--- Vector_2D/Vector_2D.py
+++ Vector_2D/Vector_2D.py
@@ -3,6 +3,7 @@ import numpy as np
 
 rep = "yes"
 while rep == "yes":
+    # input vector1
     print("To calculate vector addition\ntype v1 coordinate for example:2 3")
     while True:
         try:
@@ -17,7 +18,7 @@ while rep == "yes":
         except ValueError or NameError:
             print('The entered number is in the wrong form, try to use the correct form:2 3')
     v1 = [vx, vy]
-
+    # input vector2
     while True:
         try:
             v2x, v2y = input("Vector 2:").split()
@@ -31,26 +32,29 @@ while rep == "yes":
         except ValueError or NameError:
             print('The entered number is in the wrong form, try to use the correct form:2 3')
     v2 = [v2x, v2y]
-
+    # vector addition
     sum1 = vx + v2x
     sum2 = vy + v2y
     r = [sum1, sum2]
     magnitude = np.linalg.norm(r)
     magnitude = round(magnitude, 2)
-    origin = [0, 0]
 
+    # plot matplotlib
+    origin = [0, 0]
     fig, ax = plt.subplots()
     ax.set_xlim(-1, sum1 + 3)
     ax.set_ylim(-1, sum2 + 3)
     plt.ylabel("Y axis")
     plt.xlabel("X axis")
-
+    # plot the vector 1,2,3
     ax.quiver(origin[0], origin[1], v1[0], v1[1], angles="xy", scale_units="xy", scale=1, color="b", label=f"V1 = {v1}")
     ax.quiver(v1[0], v1[1], v2[0], v2[1], angles="xy", scale_units="xy", scale=1, color="g", label=f"V2 = {v2}")
-    ax.quiver(origin[0], origin[1], r[0], r[1], angles="xy", scale_units="xy", scale=1, color="r", label=f"V3 = {r}\nLength={magnitude}")
+    ax.quiver(origin[0], origin[1], r[0], r[1], angles="xy", scale_units="xy", scale=1, color="r",
+              label=f"V3 = {r}\nLength={magnitude}")
     plt.legend()
     plt.show()
-    plt. close("all")
+    plt.close("all")
+    # repeat? yes/no
     while True:
         rep = input("Do you want to add another vector?\nType(yes/no):")
         if rep == "no" or rep == "yes":
