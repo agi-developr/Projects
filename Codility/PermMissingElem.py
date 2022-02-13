159ed61d7947e823552486fa81f55df958fd0b9a
Ilia <agi.developr@gmail.com>
2022-02-13 17:47:50 -0500

    Create PermMissingElem.py

---
+++ Codility/PermMissingElem.py
@@ -0,0 +1,10 @@
+def solution(A):
+    if (len(A) == 0):
+        return 1
+    A.sort()
+
+    for i in range (0, len(A)):
+        if A[i] != i+1:
+            return i+1
+    return (A[len(A)-1]+1)
+    pass
